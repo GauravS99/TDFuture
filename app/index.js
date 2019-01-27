11038b88d4703cd62f47a44b31eea19f86991f65
Viresh Umbre <viresh.rajiv.umbre@gmail.com>
2019-01-26 21:47:56 -0500

    Added reset.css import

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import {App} from './components/App';
 
 import './styles/styles.css';
+import './styles/reset.css';
 
 
 ReactDOM.render(<App/>, document.getElementById('app'));
