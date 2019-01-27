541edf230e88a89edba9ce40cebce4aebca4b08e
Gaurav <g.sharma0909@gmail.com>
2019-01-26 22:57:36 -0500

    Worked on site

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Image} from 'react-bootstrap';
 
 export class Sidebar extends React.Component{
 	render(){
+		let source = require("../images/person-icon.png");
 		return (
 			<div className="sidebar colorAccent">
-				
+				<Image className="icon" src={source} circle/>
 			</div>
 		);
 	}
