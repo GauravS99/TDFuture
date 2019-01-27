45db591fe9a0e7cc813304212c24efbedb966a4f
Weiran Gu <weirangu@gmail.com>
2019-01-27 09:43:32 -0500

    Fixed predictions and updated graph.

--- app/components/Predictions.js
+++ app/components/Predictions.js
@@ -1,13 +1,5 @@
 import * as tf from '@tensorflow/tfjs';
 
-let bills = require('../toInclude/bills/model.json');
-let entertainment = require('../toInclude/entertainment/model.json');
-let food = require('../toInclude/food/model.json');
-let groceries = require('../toInclude/groceries/model.json');
-let miscellaneous = require('../toInclude/miscellaneous/model.json');
-let income = require('../toInclude/income/model.json');
-
-
 export async function getPredictions(data, model) {
 	var m
 	switch (model) {
@@ -15,19 +7,19 @@ export async function getPredictions(data, model) {
 			m = 'https://raw.githubusercontent.com/GauravS99/TDFuture/master/app/toInclude/bills/model.json';
 			break;
 		case 'entertainment':
-			m = 'https://raw.githubusercontent.com/GauravS99/TDFuture/master/app/toInclude/bills/model.json';
+			m = 'https://raw.githubusercontent.com/GauravS99/TDFuture/master/app/toInclude/entertainment/model.json';
 			break;
 		case 'food':
-			m = 'https://raw.githubusercontent.com/GauravS99/TDFuture/master/app/toInclude/bills/model.json';
+			m = 'https://raw.githubusercontent.com/GauravS99/TDFuture/master/app/toInclude/food/model.json';
 			break;
 		case 'groceries':
-			m = 'https://raw.githubusercontent.com/GauravS99/TDFuture/master/app/toInclude/bills/model.json';
+			m = 'https://raw.githubusercontent.com/GauravS99/TDFuture/master/app/toInclude/groceries/model.json';
 			break;
 		case 'miscellaneous':
-			m = 'https://raw.githubusercontent.com/GauravS99/TDFuture/master/app/toInclude/bills/model.json';
+			m = 'https://raw.githubusercontent.com/GauravS99/TDFuture/master/app/toInclude/miscellaneous/model.json';
 			break;
 		case 'income':
-			m = 'https://raw.githubusercontent.com/GauravS99/TDFuture/master/app/toInclude/bills/model.json';
+			m = 'https://raw.githubusercontent.com/GauravS99/TDFuture/master/app/toInclude/income/model.json';
 			break;
 		default:
 			m = ""
